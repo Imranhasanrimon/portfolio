ecdd5fcfd5c9039faf169269b1903e0d292d6082
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-04 17:41:33 +0600

    adding skill set

--- client-side/src/components/AboutMe.jsx
+++ client-side/src/components/AboutMe.jsx
@@ -1,9 +1,13 @@
+import MySkills from "./MySkills";
 
 const AboutMe = () => {
     return (
         <div>
             <h2 className="text-2xl md:text-3xl lg:text-5xl text-primary text-center">About Me</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi suscipit quis eaque fuga illum quaerat non recusandae soluta at obcaecati nulla quidem ipsum, maxime nesciunt excepturi molestiae sint praesentium quas.</p>
+
+            <div className="divider divider-error">Error</div>
+            <MySkills></MySkills>
 
         </div>
     );
--- client-side/src/components/MySkills.jsx
+++ client-side/src/components/MySkills.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
 const MySkills = () => {
+    const [mySkills, setMySkills] = useState([]);
+    useEffect(() => {
+        fetch('/mySkills.json')
+            .then(res => res.json())
+            .then(data => setMySkills(data))
+    }, []);
     return (
-        <div>
-            MySkills
-            MySkills
+        <div className="max-w-screen-lg mx-auto">
+            <h3>My Skills</h3>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+                {
+                    mySkills.map((skill, idx) => <div key={idx}
+                        className="flex flex-col justify-center items-center bg-customFront rounded-lg p-4"
+                    >
+                        <img src={skill.logo} alt="" />
+                        <h5>{skill.name}</h5>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
